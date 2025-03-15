4c44ea325637e015e449a1ea86d02a50b7fe5e80
AritraDey-Dev <AritraDey-Dev@users.noreply.github.com>
2025-03-15 21:01:24 +0000

    deploy: 338af1382abbd66a352cb3d7916f8835d2c1db90

---
+++ search/defines_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['e_5fstar_5fsnow_0',['E_STAR_SNOW',['../dd/d09/sipnet_8c.html#a3f70be9aae36e2c07886d14fcc89df0e',1,'sipnet.c']]],
+  ['event_5fhandler_1',['event_handler',['../dd/d25/src_2sipnet_2model_structures_8h.html#ae92ae8e64fbb4c6fa7d87cc1e93d55c0',1,'EVENT_HANDLER:&#160;modelStructures.h'],['../d9/d65/tests_2sipnet_2test__events__infrastructure_2model_structures_8h.html#ae92ae8e64fbb4c6fa7d87cc1e93d55c0',1,'EVENT_HANDLER:&#160;modelStructures.h'],['../d6/d7c/tests_2sipnet_2test__events__types_2model_structures_8h.html#ae92ae8e64fbb4c6fa7d87cc1e93d55c0',1,'EVENT_HANDLER:&#160;modelStructures.h']]],
+  ['extra_5fdata_5ftypes_2',['EXTRA_DATA_TYPES',['../de/de4/sipnet_8h.html#ab7f5280fafc469bb1061ccfc5b86e12c',1,'sipnet.h']]]
+];
